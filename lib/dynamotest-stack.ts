134a201b8ac7aca68cd89e8025c7e91c4da27b71
Koji-Sunioj <koji.gabriel218@gmail.com>
2023-01-24 18:46:05 +0200

    first

--- lib/dynamotest-stack.ts
+++ lib/dynamotest-stack.ts
@@ -1,16 +1,39 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamoDb from "aws-cdk-lib/aws-dynamodb";
+import * as apiGateway from "aws-cdk-lib/aws-apigateway";
+
+import { Construct } from "constructs";
 
 export class DynamotestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new dynamoDb.Table(this, "Test", {
+      partitionKey: { name: "itemId", type: dynamoDb.AttributeType.STRING },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DynamotestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const tableLambda = new lambda.Function(this, "ItemHandler", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "items.handler",
+      environment: { DB_NAME: table.tableName },
+    });
+
+    table.grantReadWriteData(tableLambda);
+
+    const itemApi = new apiGateway.LambdaRestApi(this, "ItemAPi", {
+      handler: tableLambda,
+      proxy: false,
+    });
+
+    const items = itemApi.root.addResource("items");
+    items.addMethod("GET");
+    items.addMethod("POST");
+
+    const item = items.addResource("{itemId}");
+    item.addMethod("GET");
+    item.addMethod("DELETE");
+    item.addMethod("PATCH");
   }
 }
